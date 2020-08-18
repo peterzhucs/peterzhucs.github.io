384b10c395f19b83ec61e0df0a63588451f2a08d
Peter Zhu <peterzhu@berkeley.edu>
2020-08-17 23:29:48 -0700

    Updates

---
+++ precache-manifest.ddd116686851b74257bfa3efbd1cce77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431b65463de03ef3109ca22410bf316b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b0a7d023ff94fe9594",
+    "url": "/static/css/2.9ef65634.chunk.css"
+  },
+  {
+    "revision": "b5b0a7d023ff94fe9594",
+    "url": "/static/js/2.721a2d00.chunk.js"
+  },
+  {
+    "revision": "a5da5d53dfba093fa63375799ffeddd6",
+    "url": "/static/js/2.721a2d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cab56b4a99400e8e46",
+    "url": "/static/js/main.e531786e.chunk.js"
+  },
+  {
+    "revision": "402421da29cdb908c009",
+    "url": "/static/js/runtime-main.c9840fbb.js"
+  },
+  {
+    "revision": "e5baeb7575615cab700936e2bf922ae5",
+    "url": "/static/media/cupcake.e5baeb75.jpg"
+  },
+  {
+    "revision": "72c8ac0a88ed09b50bb15a9abca9b9cf",
+    "url": "/static/media/cupcake2.72c8ac0a.jpg"
+  }
+]);
